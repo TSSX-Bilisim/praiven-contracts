bb2358afa71d379fa3754090fd39c98cd1940b5b
ilcann <ilcan21@itu.edu.tr>
2025-11-02 17:03:44 +0300

    fix: bump version to 1.0.5 in package.json and correct messageId type in FilterMessageJob interface

--- src/queue/types/filter-message-job.type.ts
+++ src/queue/types/filter-message-job.type.ts
@@ -2,9 +2,9 @@ import { RequestUser } from "../../auth/types/request-user.type";
 
 export interface FilterMessageJob {
   conversationId: string;
-  messageId: string;
-  content: string;
+  messageId: number;
   userId: string;
   userPayload: RequestUser;  // contracts içindeki user tipi
   modelId: string;
+  content: string;
 }

a35ee593e4a27925b68481620babd57671a63113
ilcann <ilcan21@itu.edu.tr>
2025-11-09 01:44:53 +0300

    feat: export PoliciesDeletedJob type in queue index

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,4 +1,5 @@
 export * from './types/filter-message-job.type';
 export * from './types/policy-created-job.type';
+export * from './types/policies-deleted-job.type';
 export * from './enums/job-names.enum';
 export * from './enums/queue-names.enum';

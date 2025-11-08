89f522f4325d7201e5691b3e0cf72edfa0765749
ilcann <ilcan21@itu.edu.tr>
2025-11-08 14:31:40 +0300

    feat: update version to 1.4.1 in package.json and export policy-created-job type in queue index

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,3 +1,4 @@
 export * from './types/filter-message-job.type';
+export * from './types/policy-created-job.type';
 export * from './enums/job-names.enum';
 export * from './enums/queue-names.enum';

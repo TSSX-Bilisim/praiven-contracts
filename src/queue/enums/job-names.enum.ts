6a8c7da7eae7ead1f448559000b0e1010c1b2942
ilcann <ilcan21@itu.edu.tr>
2025-11-09 01:36:41 +0300

    feat: update version to 1.5.0 in package.json, add POLICIES_DELETED enum, and create PoliciesDeletedJob type

--- src/queue/enums/job-names.enum.ts
+++ src/queue/enums/job-names.enum.ts
@@ -1,4 +1,5 @@
 export enum JobNames {
   FILTER_MESSAGE = 'filter-message',
   POLICY_CREATED = 'policy-created',
+  POLICIES_DELETED = 'policies-deleted',
 }
---
+++ src/queue/types/policies-deleted-job.type.ts
@@ -0,0 +1,3 @@
+export type PoliciesDeletedJob = {
+  policyIds: string[];
+};

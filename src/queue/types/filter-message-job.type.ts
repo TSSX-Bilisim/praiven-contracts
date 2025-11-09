cc2e2812a8111ed0ea11de860c8d69af92141478
ilcann <ilcan21@itu.edu.tr>
2025-11-09 14:17:07 +0300

    feat: update version to 1.5.3 in package.json and modify FilterMessageJob interface

--- src/queue/types/filter-message-job.type.ts
+++ src/queue/types/filter-message-job.type.ts
@@ -3,8 +3,10 @@ import { RequestUser } from "../../auth/types/request-user.type";
 export interface FilterMessageJob {
   conversationId: string;
   messageId: number;
-  user: RequestUser;  // contracts içindeki user tipi
   modelId: string;
-  systemKey: string;
   content: string;
+
+  departmentId: string;
+  roleId: string;
+  systemId: string;
 }

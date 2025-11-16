46b10257c2e6a18a0df8deb9824680a5ffa0a732
ilcann <ilcan21@itu.edu.tr>
2025-11-16 21:32:14 +0300

    feat: update version to 1.10.1 and rename metrics job enums for consistency

--- src/queue/enums/job-names.enum.ts
+++ src/queue/enums/job-names.enum.ts
@@ -3,6 +3,6 @@ export enum JobNames {
   POLICY_CREATED = 'policy-created',
   POLICIES_DELETED = 'policies-deleted',
   MESSAGE_FILTERED = 'message-filtered',
-  METRICS_INGEST_JOB = "record_metric",
-  METRICS_WS_JOB = "ws_metric",
+  METRICS_INGEST = "record_metric",
+  METRICS_WS = "ws_metric",
 }
